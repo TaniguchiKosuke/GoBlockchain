bce9bce7ff62ba5343e3e917224ac742a625a899
ti-yapitt <applekkttegg@yahoo.co.jp>
2021-12-01 23:22:44 +0900

    fix

--- wallet_server/main.go
+++ wallet_server/main.go
@@ -14,6 +14,6 @@ func main() {
 	gateway := flag.String("gateway", "http://127.0.0.1:5000", "Blockchain Gateway")
 	flag.Parse()
 
-	app := NewWalletServer(uint(*port), *gateway)
-	app.Run
+	app := NewWalletServer(uint16(*port), *gateway)
+	app.Run()
 }
